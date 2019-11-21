109022a6a634dc969e164a73b633f13b1893b5e4
Melanie W <macbookair@Melanie-W.local>
2019-11-20 22:45:35 -0700

    Updates

---
+++ precache-manifest.c8f33fd44514a14b1ae342d4318ab0ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5708d3c5c15f1395b836cd9a33680a1c",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "a8abc33d382993ac7dcd",
+    "url": "/me/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c30fd579cfc05e8b8fe4",
+    "url": "/me/static/js/2.2cc813f3.chunk.js"
+  },
+  {
+    "revision": "a8abc33d382993ac7dcd",
+    "url": "/me/static/js/main.6e94a6a7.chunk.js"
+  },
+  {
+    "revision": "9703c1e9f7cfccf1d82a",
+    "url": "/me/static/js/runtime-main.dc129b4e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/me/static/media/logo.25bf045c.svg"
+  }
+]);
